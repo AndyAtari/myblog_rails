52cc6885363220f70057eddf31769888e4f0ccf3
Andy Atari <andrewsalteri@gmail.com>
2021-05-03 17:27:20 -0400

    Added about page and styled top nav bar.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :pages
+  resources :pages 
+  get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
