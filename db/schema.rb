5b7b751783faca90030f56028d83aac5ead881e0
AndyAtari <andrewsalteri@gmail.com>
2021-05-03 17:34:37 -0400

    Generated resource for Article and migrated articles db.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_193412) do
+ActiveRecord::Schema.define(version: 2021_05_03_213102) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pages", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
