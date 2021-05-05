4efb3d378f1c3a1f44535c1791a01c4183f5f4fe
Andy Atari <andrewsalteri@gmail.com>
2021-05-05 16:08:39 -0400

    Created a new article form and on submit the article saves to db. Added flash notice on successful creation of article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,36 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def index
         @articles = Article.all 
+    end
+
+    def show
+    end
+
+    def new
+        @article = Article.new 
+    end
+
+    def create 
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
+        else
+            render "new"
+        end
+    end
+
+    private 
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params 
+        params.require(:article).permit(:title, :description)
     end
 
 end
