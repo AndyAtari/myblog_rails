aca6006c1cbb06c7b05e876b3ba10b8f929e1634
Andy Atari <andrewsalteri@gmail.com>
2021-05-13 16:13:36 -0400

    Added portfolio route in pages, and built portfolio view. Styled search bar and headers.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "pages#index"
   resources :articles
   get 'about', to: 'pages#about'
+  get 'portfolio', to: 'pages#portfolio'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
